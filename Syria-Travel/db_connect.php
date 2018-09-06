b08a2641cd5a23d850548b6691e163805c9e75d3
George Saad <george-s-94@hotmail.com>
2018-09-06 22:39:35 +0300

    remove names

--- Syria-Travel/db_connect.php
+++ Syria-Travel/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn=mysqli_connect("localhost","id2301972_syria_travel","S I dont give a shit s","id2301972_syria_travel");
+	$conn=mysqli_connect("","","","");
 	if(!$conn)
 		die("Connection Failed".mysqli_connect_error()."Error Number".mysqli_connect_errno());
 
